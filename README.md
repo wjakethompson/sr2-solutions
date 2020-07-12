05255172cecd7a25ec0c2c3f59fc5f8eca3b8da1
Jake Thompson <wjakethompson@gmail.com>
2020-07-12 13:58:12 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Solutions for *Statistical Rethinking, 2nd Edition*.
---
+++ _common.R
@@ -0,0 +1,13 @@
+set.seed(2020)
+options(digits = 3)
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  cache = TRUE,
+  out.width = "90%",
+  fig.align = "center",
+  fig.width = 6,
+  fig.asp = 0.618,
+  fig.show = "hold"
+)
