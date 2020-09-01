25cd2cd934cc222b78e0927b2dd39deef2a281cc
Jake Thompson <wjakethompson@gmail.com>
2020-08-31 20:52:55 -0500

    update _common.R with additional packages

--- _common.R
+++ _common.R
@@ -1,11 +1,18 @@
 set.seed(2020)
-options(digits = 3)
+options(digits = 3,
+        knitr.kable.NA = "")
 
 suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(rethinking))
 suppressPackageStartupMessages(library(brms))
+suppressPackageStartupMessages(library(tidybayes))
+suppressPackageStartupMessages(library(tidybayes.rethinking))
 suppressPackageStartupMessages(library(hrbrthemes))
 suppressPackageStartupMessages(library(here))
+suppressPackageStartupMessages(library(conflicted))
+
+conflict_prefer("filter", "dplyr")
+conflict_prefer("map", "purrr")
 
 ggplot2::theme_set(theme_minimal())
 
