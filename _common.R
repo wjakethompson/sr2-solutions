10e81f2298838aa9ea8c5ceaf5a659e2bed02d02
Jake Thompson <wjakethompson@gmail.com>
2020-09-18 21:59:38 -0500

    apply styling

--- _common.R
+++ _common.R
@@ -23,9 +23,17 @@ knitr::opts_chunk$set(
   dpi = 96L,
   fig.ext = "png",
   fig.width = 700 / 96,
-  fig.retina = 2L,
+  fig.retina = 3L,
   fig.asp = 1 / 1.618,
   fig.show = "hold",
   fig.align = "center",
   out.width = "70%"
 )
+
+knitr::knit_hooks$set(wrap = function(before, options, envir){
+  if (before){
+    paste0('<div class="', options$wrap, '">')
+  } else {
+    paste0('</div>')
+  }
+})
